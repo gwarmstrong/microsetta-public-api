243c996d617ecb3317386d79411761b352da3c40
George <garmstro@eng.ucsd.edu>
2020-06-02 16:44:59 -0700

    add table resource validation methods

--- microsetta_public_api/resources.py
+++ microsetta_public_api/resources.py
@@ -1,16 +1,45 @@
 import os
 import pandas as pd
+from copy import deepcopy
 from microsetta_public_api.exceptions import ConfigurationError
 from qiime2 import Artifact
 from q2_types.sample_data import AlphaDiversity, SampleData
 
 
-def _str_str_to_str_alpha(dict_of_qza_paths, resource_name):
+def _dict_of_paths_to_alpha_data(dict_of_qza_paths, resource_name):
     _validate_dict_of_qza_paths(dict_of_qza_paths,
                                 resource_name)
     new_resource = _replace_paths_with_qza(dict_of_qza_paths,
-                                           resource_name,
                                            SampleData[AlphaDiversity])
+    return new_resource
+
+
+def _transform_dict_of_table(dict_, resource_name):
+    if not isinstance(dict_, dict):
+        raise TypeError(f"Expected field '{resource_name}' to contain a "
+                        f"dictionary. Got {dict_}.")
+    new_resource = dict()
+    for table_name, attributes in dict_.items():
+        res = _transform_single_table(attributes, table_name)
+        new_resource[table_name] = res
+    return new_resource
+
+
+def _transform_single_table(dict_, resource_name):
+    _validate_dict_of_qza_paths(dict_, resource_name, allow_none=True,
+                                required_fields=['table'],
+                                non_qza_entries=['table-type']
+                                )
+    semantic_types = {
+        'table': dict_.get('table-type', "FeatureTable[Frequency]"),
+        'feature-data-taxonomy': "FeatureData[Taxonomy]",
+        'variances': "FeatureTable[Frequency]",
+    }
+    new_resource = deepcopy(dict_)
+    for key, value in dict_.items():
+        if key in semantic_types:
+            new_resource[key] = _parse_q2_data(value,
+                                               semantic_types[key])
     return new_resource
 
 
@@ -28,32 +57,52 @@ def _parse_q2_data(filepath, semantic_type):
     return data.view(pd.Series)
 
 
-def _validate_dict_of_qza_paths(dict_of_qza_paths, name):
+def _validate_dict_of_qza_paths(dict_of_qza_paths, name, allow_none=False,
+                                required_fields=None, allow_extras=False,
+                                non_qza_entries=None,
+                                ):
+    if non_qza_entries is None:
+        non_qza_entries = []
     if not isinstance(dict_of_qza_paths, dict):
         raise ValueError(f"Expected '{name}' field to contain a dict. "
                          f"Got {type(dict_of_qza_paths).__name__}")
+    if required_fields:
+        for field in required_fields:
+            if field not in dict_of_qza_paths:
+                raise ValueError(f"Did not get required field '{field}'.")
+        if not allow_extras:
+            extra_keys = list(filter(lambda x: x in set(required_fields),
+                                     dict_of_qza_paths.keys()))
+            if extra_keys:
+                raise ValueError(f"Extra keys: {extra_keys} not allowed.")
+
     for key, value in dict_of_qza_paths.items():
-        value_is_existing_qza_path = isinstance(value, str) and \
-                                     (value[-4:] == '.qza') and \
-                                     os.path.exists(value)
+        if key in non_qza_entries:
+            continue
+        is_qza = isinstance(value, str) and (value.endswith('.qza'))
+        exists = isinstance(value, str) and os.path.exists(value)
+        is_none = value is None
+        value_is_existing_qza_path = (is_qza and exists) or \
+                                     (is_none and allow_none)
 
         if not value_is_existing_qza_path:
-            raise ValueError(f'Expected existing path with .qza '
-                             f'extension. Got: {value}')
+            raise ValueError('Expected existing path with .qza '
+                             'extension. Got: {}'.format(value))
 
 
-def _replace_paths_with_qza(dict_of_qza_paths, name, semantic_type):
-    new_resource = {name: dict()}
+def _replace_paths_with_qza(dict_of_qza_paths, semantic_type):
+    new_resource = dict()
     for key, value in dict_of_qza_paths.items():
-        new_resource[name][key] = _parse_q2_data(value,
-                                                 semantic_type)
+        new_resource[key] = _parse_q2_data(value,
+                                           semantic_type)
     return new_resource
 
 
 class ResourceManager(dict):
 
     transformers = {
-        'alpha_resources': _str_str_to_str_alpha,
+        'alpha_resources': _dict_of_paths_to_alpha_data,
+        'table_resources': _transform_dict_of_table,
     }
 
     def update(self, *args, **kwargs):
@@ -77,11 +126,25 @@ class ResourceManager(dict):
 
         Examples
         --------
-        >>> resources = ResourceManager(alpha_resources={
-        ...     {'faith_pd': '/path/to/some.qza',
-        ...      'chao1': '/another/path/to/a.qza',
-        ...     }}, some_other_resource='here is a string resource')
-
+        >>> resources = ResourceManager(
+        ...     alpha_resources={
+        ...         'faith_pd': '/path/to/some.qza',
+        ...         'chao1': '/another/path/to/a.qza',
+        ...     },
+        ...     table_resources={
+        ...         'greengenes_13.8_insertion': {
+        ...             'table': '/path/to/feature-table.qza',
+        ...             'feature-data-taxonomy': '/a/feat-data-taxonomy.qza',
+        ...             'variances': '/a/variance/feature-table.qza',
+        ...         },
+        ...         'some_other_feature_table': {
+        ...             'table': '/another/path/tofeature-table.qza',
+        ...             'variances': '/a/variance/feature-table.qza',
+        ...             'table-type': 'FeatureTable[Compositional]'
+        ...         },
+        ...     }
+        ...     some_other_resource='here is a string resource',
+        ...     )
 
         """
         if len(args) == 1 and isinstance(args[0], dict):
@@ -96,11 +159,11 @@ class ResourceManager(dict):
             if resource_name in other:
                 new_resource = transformer(other[resource_name],
                                            resource_name)
-                other.update(new_resource)
+                other.update({resource_name: new_resource})
             if resource_name in kwargs:
                 new_resource = transformer(kwargs[resource_name],
                                            resource_name)
-                kwargs.update(new_resource)
+                kwargs.update({resource_name: new_resource})
 
         return super().update(other, **kwargs)
 
